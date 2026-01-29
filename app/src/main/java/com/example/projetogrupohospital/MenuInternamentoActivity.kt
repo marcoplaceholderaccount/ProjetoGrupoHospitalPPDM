b314fea98d1e009bbd657ef211520d541511526b
marco <marcomcpe72@gmail.com>
2026-01-29 11:16:55 -0100

    + Activitys repensadas

--- app/src/main/java/com/example/projetogrupohospital/MenuInternamentoActivity.kt
+++ app/src/main/java/com/example/projetogrupohospital/MenuInternamentoActivity.kt
@@ -5,16 +5,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.projetogrupohospital.databinding.ActivityMainBinding
+import com.example.projetogrupohospital.databinding.ActivityMenuInternamentoBinding
 
 class MenuInternamentoActivity : AppCompatActivity() {
+    private lateinit var janela : ActivityMenuInternamentoBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_menu_internamento)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        janela = ActivityMenuInternamentoBinding.inflate(layoutInflater)
+        setContentView(janela.root)
+
     }
 }
